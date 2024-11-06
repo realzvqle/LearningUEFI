62bdff82df1ad020ceaae686368f6d9d7274a810
realzvqle <realzvqle@gmail.com>
2024-11-06 13:41:24 -0600

    inpit

--- bootloader/main.c
+++ bootloader/main.c
@@ -9,7 +9,16 @@
 
 EFI_STATUS EfiEntry(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-    //SystemTable->BootServices->Stall(100000);
+    SystemTable->ConOut->SetMode(SystemTable->ConOut, 0);
+    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+    SystemTable->ConOut->EnableCursor(SystemTable->ConOut, TRUE);
+    while(1){
+        EFI_INPUT_KEY s;
+        SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &s);
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, &s.UnicodeChar);
+
+    }
+    //SystemTable->RuntimeServices->ResetSystem(EfiResetWarm, EFI_SUCCESS, 0, L"Error");
     while(1){continue;}
     return EFI_SUCCESS;
 }
